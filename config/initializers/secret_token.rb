fb210c37b5dbdd242a382b5f1c98c407971e5745
Mayur Soneta <mdsoneta80@gmail.com>
2013-09-12 15:25:24 -0500

    initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mango::Application.config.secret_key_base = 'ba90828a332a7e479185a52fff22826f8b21cb497efd5729d9ff031a8a72a6e96efb2ec370981073e4cd5593802ebf1b672e1a119a28b6ebf37f4f9213112691'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mango::Application.config.session_store :cookie_store, key: '_mango_session'
